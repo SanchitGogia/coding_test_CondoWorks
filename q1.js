6b3351414e6948634beca8751b771a3a619bc20d
Tlou <frl.sanchit@gmail.com>
2022-04-25 17:03:45 -0400

    Bill period

--- q1.js
+++ q1.js
@@ -4,7 +4,14 @@ fs.readFile('test-q1.txt','utf-8',(err,data)=> {
         console.log(err);
         return;
     }
-    const regex = /(?<=Customer no. - Account no.\n\s*)\d+\s\-\s\d+/;
-    console.log(data.match(regex)[0].split(' - '));
+    //customer and account number
+    //const regexCustAcc = /(?<=Customer no. - Account no.\n\s*)\d+\s\-\s\d+/;
+    //console.log(data.match(regex)[0].split(' - '));
+    
+    // Bill period
+    //const regex = /(?<=Bill period:\nService address: ).*/;
+    /*console.log(data.match(regex)[0].split(/\s\s+/));
+    const x = data.match(regex)[0].split(/\s\s+/);
+    console.log(x[x.length-1]);*/
     //console.log(data[data.search(regex)]);
 })
