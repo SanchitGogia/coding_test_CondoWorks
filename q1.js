bb5ba46edce599ce916bf2678843ccbb3edf87f4
Tlou <frl.sanchit@gmail.com>
2022-04-25 18:50:55 -0400

    Question 1 final

--- q1.js
+++ q1.js
@@ -1,24 +1,36 @@
 const fs = require('fs');
-fs.readFile('test-q1.txt','utf-8',(err,data)=> {
-    if (err){
-        console.log(err);
-        return;
-    }
-    //customer and account number
-    //const regexCustAcc = /(?<=Customer no. - Account no.\n\s*)\d+\s\-\s\d+/;
-    //console.log(data.match(regex)[0].split(' - '));
-    
-    // Bill period
-    //const regex = /(?<=Bill period:\nService address: ).*/;
-    /*console.log(data.match(regex)[0].split(/\s\s+/));
-    const x = data.match(regex)[0].split(/\s\s+/);
-    console.log(x[x.length-1]);*/
-    //console.log(data[data.search(regex)]);
+let customerNumber,accountNumber,billPeriod,billDate,billNumber,charge;
+function fileparse(filename)
+{   fs.readFile(filename,'utf-8',(err,data)=> {
+        if (err){
+            console.log("Invalid file name");
+            return;
+        }
+        //customer and account number regex
+        const regexCustAcc = /(?<=Customer no. - Account no.\n\s*)\d+\s\-\s\d+/;
+        
+        const customerandacc = data.match(regexCustAcc)[0].split(' - ') //array to store customer and account number
+        customerNumber = customerandacc[0];
+       
+        accountNumber =  customerandacc[1];
+        // Bill period regex
+        const BP = /(?<=Bill period:\nService address: ).*/;
+        
+        const x = data.match(BP)[0].split(/\s\s+/);
+        billPeriod = x[x.length-1];
+        
 
-    // Bill number, date and total charges
-    
-    const regex1 = /(?<=Bill date: ).*/;
-    const regex2 = /(?<=Bill number: )\d+/;
-    const regex3 = /(?<=Total new charges\s+)[^\s]+/;
-    console.log(`${data.match(regex1)[0]} ${data.match(regex2)[0]} ${data.match(regex3)[0]}`);
-})
+        // Bill number, date and total charges
+        
+        const dateRegex = /(?<=Bill date: ).*/;
+        const numRegex = /(?<=Bill number: )\d+/;
+        const chargeRegex = /(?<=Total new charges\s+)[^\s]+/;
+        billDate = data.match(dateRegex)[0];
+        billNumber = data.match(numRegex)[0];
+        charge = data.match(chargeRegex)[0];
+        printData();
+    })}
+function printData(){
+    console.log(`Customer Number: ${customerNumber}\nAccount Number: ${accountNumber}\nBill Period: ${billPeriod}\nBill Number: ${billNumber}\nBill Date: ${billDate}\nTotal new charges: ${charge}`);
+}
+    fileparse(process.argv[2]);
