7925031a71e32153717485d7b66374b7232686e0
Tlou <frl.sanchit@gmail.com>
2022-04-25 17:30:26 -0400

    Completion for bill date, number and charge

--- q1.js
+++ q1.js
@@ -14,4 +14,11 @@ fs.readFile('test-q1.txt','utf-8',(err,data)=> {
     const x = data.match(regex)[0].split(/\s\s+/);
     console.log(x[x.length-1]);*/
     //console.log(data[data.search(regex)]);
+
+    // Bill number, date and total charges
+    
+    const regex1 = /(?<=Bill date: ).*/;
+    const regex2 = /(?<=Bill number: )\d+/;
+    const regex3 = /(?<=Total new charges\s+)[^\s]+/;
+    console.log(`${data.match(regex1)[0]} ${data.match(regex2)[0]} ${data.match(regex3)[0]}`);
 })
