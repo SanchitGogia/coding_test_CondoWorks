6abfecfde27c423b16adb6aec4ce4c0675ec823b
Sanchit Gogia <gogi2580@mylaurier.ca>
2022-04-26 03:33:31 -0400

    Download functionality added

--- q2.js
+++ q2.js
@@ -1,6 +1,9 @@
 const puppeteer = require('puppeteer');
+const path = require("path");
+const outputPath = path.resolve('./invoice');
 async function browserInst(){
-    const browser =  await puppeteer.launch({headless: false, slowMo: 75, defaultViewport: null});
+    
+    const browser =  await puppeteer.launch({headless: true, slowMo: 75, defaultViewport: null});
     let page = await browser.newPage();
     await page.goto('https://app-dev.condoworks.co/');
     //return browser;
@@ -15,26 +18,30 @@ async function browserInst(){
     await page.goto('https://app-dev.condoworks.co/invoices/all');
     await page.waitForSelector('[name="invoices.InvoiceNumber"]');
     await page.type('[name="invoices.InvoiceNumber"]','123');
+    const handle1 = await page.$('#grid4e095c51a3223459378644ddcd24b1473');
+    const handlearray = await handle1.$$('[role="row"]');
+
+    console.log("hello");
+    //console.log(sizeof(handlearray));
+    handlearray.forEach(async (element)=>{
+        let handleval = await element.$('[title="123444"]');
+        if(handleval!= null){
+            const buttonEl = await element.$('[title="View/Edit"]');
+            //console.log(buttonEl);
+            const invoiceLink = await page.evaluate(anchor => anchor.getAttribute('href'), buttonEl);
+            await page.goto(invoiceLink);
+            await page._client.send('Page.setDownloadBehavior',{
+                behavior: 'allow',
+                downloadPath: outputPath
+            });
+            await page.click('[title="Download file"]');
+            await page.waitForTimeout(10000);
+            browser.close();
+
+        }
+        //browser.close();
+    })
     
-    console.log("hello");
-    //const htmlString = await page.content();
-    //console.log(htmlString);
-    /*await Promise.all([
-        page.type('#gs_invoices.InvoiceNumber','123'),
-        page.waitForNavigation(),
-        ]);*/
-    console.log("hello");
-    //const f =  page.frames();
-    //console.log(f[0]);
-    //const textfield = await f.$('#gs_invoices.InvoiceNumber');
-    //console.log('textfield');
-    //await page.type('#gs_invoices.InvoiceNumber','123');
-    /*data = await page.evaluate(()=>{
-        let results = []
-        let items = document.querySelectorAll('.jqgrow ui-row-ltr')
-        items.forEach((item)=>{
-            console.log(item);
-        })
-    })*/
+    
 }
 browserInst();
